22a07ed414781ce795563de55ec801726935c00a
Zhi Sun <inhzus@gmail.com>
2019-09-19 19:36:47 +0800

    [Feat] battery block

--- src/battery_block.cc
+++ src/battery_block.cc
@@ -3,8 +3,20 @@
 //
 
 #include "block.h"
+#include "utils.h"
+
+#include <cstring>
+#include <regex>
 
 char const *BLOCK_NAME_POWER = "power";
+
+bool caseEqual(const std::string &l, const std::string &r) {
+  return std::equal(
+      l.begin(), l.end(), r.begin(), r.end(),
+      [](char a, char b) {
+        return tolower(a) == tolower(b);
+      });
+}
 
 PowerBlock::PowerBlock() :
     Block(BLOCK_NAME_POWER), status_(), percent_() {
@@ -12,6 +24,32 @@ PowerBlock::PowerBlock() :
 }
 
 void PowerBlock::update() {
+  FILE *fp = popen("acpi -b", "r");
+  int fd = fileno(fp);
+  std::string s = readline(fd);
+  if (s.empty()) {
+    return;;
+  }
+  char bat[20];
+  int index;
+  std::regex pattern("\\d: ([^,]+), (\\d+)"); // NOLINT
+  // (modernize-raw-string-literal)
+  std::smatch match;
+  std::regex_search(s, match, pattern);
+  std::string status = match[1].str();
+  percent_ = std::stoi(match[2].str());
+  if (caseEqual(status, "Discharging") == 0) {
+    status_ = POWER_BAT;
+  } else if (caseEqual(status, "Unknown") == 0) {
+    status_ = POWER_BAT;
+  } else if (caseEqual(status, "Charging") == 0) {
+    status_ = POWER_AC;
+  } else if (caseEqual(status, "Full") == 0) {
+    status_ = POWER_FULL;
+  } else {
+    status_ = POWER_BAT;
+  }
+  pclose(fp);
 }
 
 std::string PowerBlock::format() {
@@ -20,7 +58,7 @@ std::string PowerBlock::format() {
     case POWER_AC:c = "\uF1E6";
       break;
     case POWER_BAT:
-    case POWER_FULL:c = "\uF1E6";
+    case POWER_FULL:c = "\uF213";
       break;
   }
   char const *format = "%s %d%%%%";
